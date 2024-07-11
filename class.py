b8207ae19fca368693a8d7304167e7f6cf6f6c9d
Fay3l <bonboui12@gmail.com>
2024-07-11 23:53:37 +0200

    ImageFont.truetype error

---
+++ class.py
@@ -0,0 +1,47 @@
+from dataclasses import dataclass
+from typing import Tuple,List
+from PIL import Image, ImageDraw, ImageFont
+
+
+@dataclass
+class Response:
+    clues:str
+    language:str
+    person:str
+    theme:str
+    name:str
+
+@dataclass
+class TextData:
+    content: str
+    position: Tuple[int, int]  # (x, y) position
+    font_size: int
+    color: Tuple[int, int, int]  # (R, G, B) color
+    align: str 
+
+def create_instagram_story(texts: List[TextData], output_path: str):
+    # Créer une image vierge
+    image = Image.new('RGB', (1080, 1350), color=(0, 0, 0))
+
+    draw = ImageDraw.Draw(image)
+
+    # Ajouter les textes à l'image
+    for text in texts:
+        try:
+            font = ImageFont.truetype(font = "armn", size=text.font_size)
+        except IOError:
+            font = ImageFont.load_default()
+            print(IOError)
+        draw.text(text.position, text.content, font=font, fill=text.color,align=text.align)
+
+    # Enregistrer l'image
+    image.save(output_path)
+
+# Exemple d'utilisation
+res = Response(name="Jeu de devinette",clues="",language="fr",person="",theme="Sport")
+
+texts = [
+    TextData(content=res.name, position=(540, 675), font_size=50, color=(255, 255, 255),align="center"),
+]
+
+create_instagram_story(texts, "instagram_story.png")
