2f36031bd3764ac2ff1492edbea87d7e1d1ce6cc
Fay3l <bonboui12@gmail.com>
2024-08-15 18:19:44 +0200

    Image

--- api.py
+++ api.py
@@ -1,13 +1,13 @@
 from mistralai import Mistral
 import os
 from dotenv import load_dotenv
-import re
+from typing import List
 import json
-
-from classes import Clues, Response
-
-load_dotenv()
+from PIL import Image, ImageDraw, ImageFont
+from classes import Clues, Response, ContentData, TemplateData
 from mistralai import Mistral
+load_dotenv()
+
 
 api_key = os.getenv("MISTRALAPI_KEY")
 model = "open-mistral-7b"
@@ -69,9 +69,46 @@ try:
 except json.JSONDecodeError as e:
     print(f"Erreur lors de la conversion du JSON: {e}")
 
-print(json_loads)
+response_data = Response(clues=json_loads['clues'],name=json_loads['reponse'])
 
-clues_list = [Clues(**clue) for clue in json_loads['clues']]
-response_data = Response(clues=clues_list, reponse=json_loads['reponse'])
 
-print(response_data)
+def create_instagram_story(template: List[TemplateData]):
+    # Créer les images vierges
+    for image in template:
+        new_image = Image.new('RGB', (1080, 1920), color=(217, 217, 217))
+        draw = ImageDraw.Draw(new_image)
+        # Ajouter les textes à l'image
+        for text in image.content:
+            try:
+                if(text.image):
+                    foreground_image = Image.open(text.image)
+                    foreground_image = foreground_image.resize((150, 150))  # Redimensionner si nécessaire    
+                    position = (300, 200)
+                font = ImageFont.truetype(font = text.fonts, size=text.font_size)
+            except IOError:
+                font = ImageFont.load_default()
+                print(IOError)
+            draw.text(text.position, text.clue_text, font=font, fill=text.color,align=text.align)
+    # Enregistrer l'image
+        new_image.save(image.path)
+
+def open_instagram_story(content: List[ContentData]):
+    # Créer les images vierges
+        # Ajouter les textes à l'image
+    for count,data in enumerate(content):
+        if data.language == "fr":
+            template = Image.open(f"./template/{data.language}/{count+1}.jpg")
+            draw = ImageDraw.Draw(template)
+        
+        try:
+            font = ImageFont.truetype(font = data.fonts, size=data.font_size)
+        except IOError:
+            font = ImageFont.load_default()
+            print(IOError)
+        
+        draw.text(data.position, data.clue_text, font=font, fill=data.color,align=data.align)
+        template.save(f"Clue_{count}_{data.language}.jpg")
+    
+
+
+print(response_data.clues)
--- classes.py
+++ classes.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass
 from typing import Tuple,List,Optional
-from PIL import Image, ImageDraw, ImageFont
+
 
 @dataclass
 class Clues:
@@ -38,48 +38,7 @@ class TemplateData:
 
 
 
-def create_instagram_story(template: List[TemplateData]):
-    # Créer les images vierges
-    for image in template:
-        new_image = Image.new('RGB', (1080, 1920), color=(217, 217, 217))
-        draw = ImageDraw.Draw(new_image)
-        # Ajouter les textes à l'image
-        for text in image.content:
-            try:
-                if(text.image):
-                    foreground_image = Image.open(text.image)
-                    foreground_image = foreground_image.resize((150, 150))  # Redimensionner si nécessaire    
-                    position = (300, 200)
-                font = ImageFont.truetype(font = text.fonts, size=text.font_size)
-            except IOError:
-                font = ImageFont.load_default()
-                print(IOError)
-            draw.text(text.position, text.clue_text, font=font, fill=text.color,align=text.align)
-    # Enregistrer l'image
-        new_image.save(image.path)
-
-def open_instagram_story(content: List[ContentData]):
-    # Créer les images vierges
-        # Ajouter les textes à l'image
-    for count,data in enumerate(content):
-        if data.language == "fr":
-            template = Image.open(f"./template/{data.language}/{count+1}.jpg")
-            draw = ImageDraw.Draw(template)
-        
-        try:
-            font = ImageFont.truetype(font = data.fonts, size=data.font_size)
-        except IOError:
-            font = ImageFont.load_default()
-            print(IOError)
-        
-        draw.text(data.position, data.clue_text, font=font, fill=data.color,align=data.align)
-        template.save(f"Clue_{count}_{data.language}.jpg")
-    
 
 
 
-content=[ContentData(clue_text="Jeu\n\nde\n\ndevinettes",language="fr", position=(210, 800),image="",clue_number=0, font_size=140, color=(0, 0, 0),align="center",fonts="./fonts/Sans.ttf"),
-        ContentData(clue_text="Qui suis-je ?",language="fr", position=(300, 1600),image="",clue_number=0 ,font_size=80, color=(0, 0, 0),align="center",fonts="./fonts/Sans.ttf"),]
 
-# create_instagram_story(images)
-open_instagram_story(content)
